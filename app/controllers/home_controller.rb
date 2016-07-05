8db3dc5308242b8379061d1e0ed28027bd56e2d4
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 17:56:56 -0300

    Create constant URI_WEBMOTORS_BRANDS in model Make and refactoring HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
   def index
-    #search the make
-    uri = URI("http://www.webmotors.com.br/carro/marcas")
-
     # Make request for Webmotors site
-    response = Net::HTTP.post_form(uri, {})
+    response = Net::HTTP.post_form(Make::URI_WEBMOTORS_BRANDS, {})
     json = JSON.parse response.body
 
     # Itera no resultado e grava as marcas que ainda não estão persistidas
--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,3 @@
 class Make < ActiveRecord::Base
+  URI_WEBMOTORS_BRANDS = URI('http://www.webmotors.com.br/carro/marcas')
 end
