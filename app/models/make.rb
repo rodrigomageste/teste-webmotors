22e1e884dfb5a15c24bf7022a24f9c9b73353edc
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 18:52:28 -0300

    Create method Make.create_entries_from_webmotors_api

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,14 @@
 class Make < ActiveRecord::Base
   URI_WEBMOTORS_BRANDS = URI('http://www.webmotors.com.br/carro/marcas')
+
+  def self.create_entries_from_webmotors_api
+    web_motors_response = Net::HTTP.post_form(URI_WEBMOTORS_BRANDS, {})
+    car_brands = JSON.parse(web_motors_response.body)
+
+    return if Make.count == car_brands.uniq.count
+
+    car_brands.each do |brand|
+      Make.find_or_create_by(name: brand["Nome"], webmotors_id: brand["Id"])
+    end
+  end
 end
---
+++ spec/models/make_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Make do
+  describe '.create_entries_from_webmotors_api' do
+    it 'not run find_or_create_by if all brands already are created' do
+      expect(Make).to receive(:count) { 178 }
+      expect(Make).not_to receive(:find_or_create_by)
+      Make.create_entries_from_webmotors_api
+    end
+
+    it 'create car brands if database is empty' do
+      Make.create_entries_from_webmotors_api
+      expect(Make.count).to eq(178)
+    end
+  end
+end
