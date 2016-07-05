8134a0e8496b5b535b26a4d26ae82123fa475e93
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 20:43:45 -0300

    Refactoring ModelsController to user method Make#create_models_from_webmotors_api

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,15 +1,6 @@
 class ModelsController < ApplicationController
   def index
     make = Make.find_by(webmotors_id: params[:webmotors_make_id])
-
-    response = Net::HTTP.post_form(Model::URI_WEBMOTORS_MODELS, { marca: make.webmotors_id })
-    models_json = JSON.parse response.body
-
-    # Itera no resultado e grava os modelos que ainda não estão persistidas
-    models_json.each do |json|
-      if Model.where(name: json["Nome"], make_id: make.id).size == 0
-        Model.create(make_id: make.id, name: json["Nome"])
-      end
-    end
+    make.create_models_from_webmotors_api
   end
 end
