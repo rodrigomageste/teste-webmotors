41e7a9f52ef0b92986ac2c0e0fb12c29e3e3ecd4
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 17:03:27 -0300

    Removed debuggers in Home and Models controllers

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -9,8 +9,6 @@ class ModelsController < ApplicationController
     response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
     models_json = JSON.parse response.body
 
-    # debugger
-
     # Itera no resultado e grava os modelos que ainda não estão persistidas
     models_json.each do |json|
       if Model.where(name: json["Nome"], make_id: make.id).size == 0
