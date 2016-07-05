70e262147140853bc4296f4e93ab393db5ea632c
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 18:55:12 -0300

    Refactor action index at HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # Make request for Webmotors site
-    response = Net::HTTP.post_form(Make::URI_WEBMOTORS_BRANDS, {})
-    json = JSON.parse response.body
-
-    # Itera no resultado e grava as marcas que ainda não estão persistidas
-    json.each do |make_params|
-      Make.find_or_create_by(name: make_params["Nome"], webmotors_id: make_params["Id"])
-    end
+    Make.create_entries_from_webmotors_api
   end
 end
