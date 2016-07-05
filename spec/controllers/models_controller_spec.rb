5f60e7633603c90ff8643a5c76a729b443ed8b12
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-05 20:07:23 -0300

    Make test for ModelsController

---
+++ spec/controllers/models_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ModelsController do
+  describe 'GET index' do
+    let(:uri) { URI("http://www.webmotors.com.br/carro/modelos") }
+    let(:make) { Make.create(name: "CHEVROLET", webmotors_id: 2) }
+
+    it 'create models for a make' do
+      get :index, webmotors_make_id: make.webmotors_id
+
+      webmotors_response = Net::HTTP.post_form(uri, { marca: make.webmotors_id })
+      models_json = JSON.parse webmotors_response.body
+
+      expect(make.models.count).to eq(models_json.count)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
