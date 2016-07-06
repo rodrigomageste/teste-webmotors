19cf7d69b50ab1daddebc573aa9cafa310c2729e
Rodrigo Mageste <rodrigo.mgst@gmail.com>
2016-07-06 10:11:04 -0300

    Feature test to show flow of application

---
+++ spec/features/application_flow_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Application flow' do
+  describe 'show all models when choose one make and click in button' do
+    it 'success' do
+      visit root_path
+      select('CHEVROLET', from: 'Escolha a Fabricante:')
+      click_button 'Buscar modelos'
+
+      expect(page).to have_content('ASTRA')
+      expect(page).not_to have_content('FIESTA')
+    end
+  end
+end
